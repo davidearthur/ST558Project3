d0d02dd678277cd97f344ede5500ac8f487855c5
David Arthur <davidearthur@gmail.com>
2021-07-27 20:21:36 -0400

    Data page mostly done, some plotting and modeling

--- server.R
+++ server.R
@@ -6,11 +6,16 @@
 #
 #    http://shiny.rstudio.com/
 #
-
-library(shiny)
+# 
+library(shinydashboard)
 library(DT)
 library(MASS)
 library(tidyverse)
+library(GGally)
+library(plotly)
+
+filter
+
 select <- dplyr::select
 
 heartData <- read_csv("processed.cleveland.data",
@@ -20,64 +25,129 @@ heartData <- read_csv("processed.cleveland.data",
                                     ),
                       na = "?"
 )
-    
-heartData <- heartData %>% 
-    mutate(across(c(sex, cp, fbs, restecg, exang, thal), as_factor)) %>%
-    mutate(across(c(slope, ca, num), ordered)) %>%
-    na.omit()
 
+heartData <- heartData %>%
+  mutate(across(c(sex, cp, fbs, restecg, exang, slope, thal), as_factor)) %>%
+  mutate(across(c(ca, num), ordered)) %>%
+  mutate(disease = factor(num != 0)) %>%
+  select(disease, everything())
+  na.omit()
 
-str(heartData)
+fig3d <- plot_ly(x = heartData$trestbps, y = heartData$age, z = heartData$num)
+fig3d
 
-binMod <- glm(num ~ ., family = binomial, heartData)
+figBox <- plot_ly(heartData, y = ~trestbps, color = ~disease, type = "box")
+figBox
+
+figBar <- heartData %>% count(cp, disease) %>%
+  plot_ly(x = ~cp, y = ~n, color = ~disease) %>%
+  add_bars()
+figBar
+
+ggplot(heartData, aes(x = slope, fill = num)) + geom_bar()
+# 
+# ggpairs(heartData)
+# 
+# str(heartData)
+# 
+binMod <- glm(num ~ . - disease, family = binomial, heartData)
 summary(binMod)
 df <- data.frame(binMod$fitted.values, heartData$num)
+tabBin <- xtabs(~ (binMod$fitted.values > .4) + (heartData$num != 0))
+tabBin
+(tabBin[1,1] + tabBin[2,2])/sum(tabBin)
+library(ROCR)
+predBin <- prediction(binMod$fitted.values, (heartData$num != 0))
+perfBin <- performance(predBin, "acc")
+plot(perfBin)
+optimize(perfBin, interval = c(.2, .8), maximum = TRUE)
+max(perfBin)
+perfBin
+slotNames(perfBin)
+perfBin @ x.name
+
+aucBin <- performance(predBin, "auc")
+aucBin @ y.values[[1]]
+perfBin @ y.values[[1]]
 
 
-orderedLogMod <- polr(num ~ ., heartData)
-orderedLogMod
+binMod2 <- glm(disease ~ . - num, family = binomial, heartData)
+summary(binMod2)
+df <- data.frame(binMod2$fitted.values, heartData$num)
+tabBin <- xtabs(~ (binMod2$fitted.values > .4) + (heartData$num != 0))
+tabBin
+(tabBin[1,1] + tabBin[2,2])/sum(tabBin)
+library(ROCR)
+predBin <- prediction(binMod2$fitted.values, (heartData$num != 0))
+perfBin <- performance(predBin, "acc")
+plot(perfBin)
+optimize(perfBin, interval = c(.2, .8), maximum = TRUE)
+max(perfBin)
+perfBin
+slotNames(perfBin)
+perfBin @ x.name
 
-pred <- predict(orderedLogMod, heartData, "class")
-df2 <- data.frame(pred, heartData$num)
-df2
-tab <- xtabs(~ (pred == 0) + (heartData$num == 0))
-(tab[1,1] + tab[2,2])/sum(tab)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
-})
-
-options(contrasts = c("contr.treatment", "contr.poly"))
-house.plr <- polr(Sat ~ Infl + Type + Cont, weights = Freq, data = housing)
-house.plr
-summary(house.plr, digits = 3)
-
-## slightly worse fit from
-summary(update(house.plr, method = "probit", Hess = TRUE), digits = 3)
-## although it is not really appropriate, can fit
-summary(update(house.plr, method = "loglog", Hess = TRUE), digits = 3)
-summary(update(house.plr, method = "cloglog", Hess = TRUE), digits = 3)
-
-predict(house.plr, housing, type = "p")
-addterm(house.plr, ~.^2, test = "Chisq")
-house.plr2 <- stepAIC(house.plr, ~.^2)
-house.plr2$anova
-anova(house.plr, house.plr2)
-
-house.plr <- update(house.plr, Hess=TRUE)
-pr <- profile(house.plr)
-confint(pr)
-plot(pr)
-pairs(pr)
+aucBin <- performance(predBin, "auc")
+aucBin @ y.values[[1]]
+perfBin @ y.values[[1]]
+# 
+# orderedLogMod <- polr(num ~ ., heartData)
+# orderedLogMod
+# 
+# pred <- predict(orderedLogMod, heartData, "class")
+# df2 <- data.frame(pred, heartData$num)
+# df2
+# tab <- xtabs(~ (pred == 0) + (heartData$num == 0))
+# (tab[1,1] + tab[2,2])/sum(tab)
+# 
+# # Define server logic required to draw a histogram
+function(input, output, session) {
+  filterTableData <- reactive({
+    filteredTableData <- heartData %>% select(input$tableVariables)
+  })
+  output$fullTable <- renderDT({
+    datatable(filterTableData(),
+              options = list(scrollX = TRUE))
+  })
+  output$dlTable <- downloadHandler(
+    filename = "HeartData.csv",
+    content = function(con) {
+      write.csv(filterTableData(), con)
+    }
+  )
+# 
+#     output$distPlot <- renderPlot({
+# 
+#         # generate bins based on input$bins from ui.R
+#         x    <- faithful[, 2]
+#         bins <- seq(min(x), max(x), length.out = input$bins + 1)
+# 
+#         # draw the histogram with the specified number of bins
+#         hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# 
+#     })
+# 
+}
+# 
+# options(contrasts = c("contr.treatment", "contr.poly"))
+# house.plr <- polr(Sat ~ Infl + Type + Cont, weights = Freq, data = housing)
+# house.plr
+# summary(house.plr, digits = 3)
+# 
+# ## slightly worse fit from
+# summary(update(house.plr, method = "probit", Hess = TRUE), digits = 3)
+# ## although it is not really appropriate, can fit
+# summary(update(house.plr, method = "loglog", Hess = TRUE), digits = 3)
+# summary(update(house.plr, method = "cloglog", Hess = TRUE), digits = 3)
+# 
+# predict(house.plr, housing, type = "p")
+# addterm(house.plr, ~.^2, test = "Chisq")
+# house.plr2 <- stepAIC(house.plr, ~.^2)
+# house.plr2$anova
+# anova(house.plr, house.plr2)
+# 
+# house.plr <- update(house.plr, Hess=TRUE)
+# pr <- profile(house.plr)
+# confint(pr)
+# plot(pr)
+# pairs(pr)
--- ui.R
+++ ui.R
@@ -7,27 +7,70 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+library(shinydashboard)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("About", tabName = "about", icon = icon("info")),
+    menuItem("Data", tabName = "data", icon = icon("table")),
+    menuItem("Data Exploration", tabName = "dataExploration", icon = icon("chart-bar")),
+    menuItem("Modeling", tabName = "modeling", icon = icon("chart-line"),
+             menuSubItem("Modeling Info", tabName = "info"),
+             menuSubItem("Model Fitting", tabName = "fitting"),
+             menuSubItem("Prediction", tabName = "prediction")
+             )
+  )
+)
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+body <- dashboardBody(
+  tabItems(
+    tabItem("about", h2("About")
+            ),
+    tabItem("data", h2("Data"),
+            fluidRow(
+              box(
+                width = 2,
+                checkboxGroupInput("tableVariables", "Variables to include in table",
+                                 choices = names(heartData),
+                                 inline = FALSE,
+                                 selected = names(heartData)
+                                 )
+                ),
+              box(
+                width = 10,
+                DTOutput("fullTable")#,
+                # style = "overflow-x: scroll"
+                )
+              ),
+            fluidRow(
+              downloadButton("dlTable", "Download data set with selected variables"))
+            ),
+    tabItem("dataExploration", h2("Data Exploration")
+            ),
+    tabItem("info", h2("Modeling Info")
+            ),
+    tabItem("fitting", h2("Model Fitting")
+            ),
+    tabItem("prediction", h2("Prediction")
+            )
+  )
+)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+dashboardPage(
+  dashboardHeader(title = "Heart Disease Data"),
+  sidebar,
+  # dashboardSidebar(
+  #     sidebarMenu(
+  #         menuItem("About", tabName = "about", icon = icon("info")),
+  #         menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+  #         menuItem("Widgets", icon = icon("th"), tabName = "widgets"),
+  #         menuItem("ChartsTest", icon = icon("bar-chart-o"), startExpanded = TRUE,
+  #                  menuSubItem("Sub-item 1", tabName = "subitem1"),
+  #                  menuSubItem("Sub-item 2", tabName = "subitem2")
+  #         )
+  #     ),
+  #     textOutput("res")
+  # ),
+  body
+)
